cbc0edcf39776fd336c4701bbb7b57d1dd15fe7e
duwei <905571995@qq.com>
2016-08-27 11:15:11 +0800

    第三方库整理

--- src/app/components/medium_editor/editor.directive.js
+++ src/app/components/medium_editor/editor.directive.js
@@ -1,4 +1,4 @@
-export function mediumEditor(BASEURL, $localStorage, $sessionStorage, ApiService, Utils) {
+export function mediumEditor() {
   'ngInject';
   let directive = {
     restrict: 'E',
@@ -80,99 +80,10 @@ export function mediumEditor(BASEURL, $localStorage, $sessionStorage, ApiService
     });
   }
 
-  function initMediumInsert(eleObj, editor, enabled = true) {
-    eleObj.mediumInsert({
-      editor: editor,
-      enabled: enabled,
-      addons: {
-        images: {
-          // $ele, data
-          uploadCompleted: function() {
-            Utils.toast('success', '上传图片成功!');
-          },
-          // uploadErrors, data
-          uploadFailed: function(uploadErrors) {
-            Utils.toast('error', uploadErrors)
-          },
-          label: '<md-icon class="material-icons">image</md-icon>',
-          deleteScript: null,
-          preview: true,
-          captions: true,
-          captionPlaceholder: '图片标题',
-          autoGrid: 2,
-          fileUploadOptions: {
-            headers: {
-              token: $localStorage.token || $sessionStorage.token
-            },
-            url: `${BASEURL}upload/image`,
-            type: 'POST',
-            paramName: 'image',
-            dataType: 'json',
-            acceptFileTypes: /(\.|\/)(gif|jpe?g|png)$/i,
-            // 最大500K
-            maxFileSize: 500 * 1024
-          },
-          styles: {
-            wide: {
-              label: '<i class="material-icons">format_align_justify</i>'
-            },
-            left: {
-              label: '<i class="material-icons">format_align_left</i>'
-            },
-            right: {
-              label: '<i class="material-icons">format_align_right</i>'
-            },
-            grid: {
-              label: '<i class="material-icons">format_align_center</i>'
-            }
-          },
-          actions: {
-            remove: {
-              label: '<i class="material-icons">close</i>',
-              clicked: function($ele) {
-                let fileName = $ele.attr('src').split('/').pop();
-                ApiService.delete(`upload/${fileName}`)
-                  .then(res => {
-                    Utils.toast('success', res.message);
-                  })
-                  .finally(() => {
-                    $ele.remove();
-                  });
-              }
-            }
-          },
-          messages: {
-            acceptFileTypesError: '不支持此格式的图片！',
-            maxFileSizeError: '图片太大！'
-          }
-        },
-        embeds: {
-          label: '<md-icon class="material-icons">movie</md-icon>',
-          placeholder: '粘贴视频地址，然后enter',
-          captions: true,
-          captionPlaceholder: '视频标题',
-          // oembedProxy: 'http://medium.iframe.ly/api/oembed?iframe=1',
-          styles: {
-            wide: {
-              label: '<i class="material-icons">format_align_justify</i>'
-            },
-            left: {
-              label: '<i class="material-icons">format_align_left</i>'
-            },
-            right: {
-              label: '<i class="material-icons">format_align_right</i>'
-            }
-          }
-        }
-      }
-    });
-  }
-
   function linkFunc(scope, ele) {
     //初始化编辑器
     let editor = initMedium(ele);
     //初始化编辑器insert插件
-    initMediumInsert(angular.element(ele), editor);
 
     scope.exprotEditor = {
       getHTML: () => {
